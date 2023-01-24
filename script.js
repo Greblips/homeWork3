803823ad6c00b4a335406bd52a64d9994f26440f
Greblips <Evgenspilbegrg@gmail.com>
2023-01-24 10:51:59 +0300

    Complite homeWork

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const access = (password === password2) ? "Пароль введен верно"
 alert(access);
 
 
-// Задача 2
+ // Задача 2
 
 const c = prompt('Введите чиcло от 0 до 10');
-const comparison = Number((c > 0 && c <= 10))? 'Верно': 'Неверно';
+const comparison = Number((c > 0 && c < 10))? 'Верно': 'Неверно';
 console.log(comparison);
 
-// Задание 3
+ // Задание 3
 
 const d = 110;
 const e = 10;
@@ -24,7 +24,7 @@ if (d > 100 || e > 100) {
 }
 
 
-// Задание 4
+// // Задание 4
 
 let a = '2';
 let b = '3';
@@ -63,3 +63,39 @@ switch (monthNumber) {
         default:
           alert( 'нет больше 12 месяцев' );
       }
+
+//     //   Задание 6
+//     https://github.com/Greblips/homeWork1/pull/3
+
+    // Задание 7
+
+     const checkOdd = prompt('Введите число')
+     if( !isNaN(checkOdd) ){
+      if( checkOdd % 2 === 0) alert(`Число ${checkOdd} четное`);
+      else alert(`Число ${checkOdd} нечетное`)
+     }
+     else alert (`${checkOdd} -Это не число`)
+
+
+   //   Задание 8
+
+   let clientOS = prompt ('Ведите Операционную систему 0 — iOS, 1 — Android');
+   if (clientOS == 0 || clientOS == 1){
+      if (clientOS == 0) console.log("Установите версию приложения для iOS по ссылке")
+      else console.log("Установите версию приложения для Android по ссылке")
+   }
+   else alert ("Ввели некорректные данные")
+
+   // задание 9
+
+
+   clientOS = prompt ('Ведите Операционную систему 0 — iOS, 1 — Android');
+   let yearCreateTelephone =  prompt ('Год выпуска телефона');
+   if ((clientOS == 0 || clientOS == 1) && !isNaN(yearCreateTelephone)){
+      if (clientOS == 0 && yearCreateTelephone>=2015) console.log("Установите версию приложения для iOS по ссылке")
+      else if (clientOS == 0 && yearCreateTelephone<=2015) console.log('Установите облегченную версию приложения для iOS по ссылке')
+      else if (clientOS == 1 && yearCreateTelephone>=2015) console.log('Установите версию приложения для Android по ссылке')
+      else if (clientOS == 1 && yearCreateTelephone<=2015) console.log('Установите облегченную версию приложения для Android по ссылке')
+   }
+   else alert ("Ввели некорректные данные")
+
